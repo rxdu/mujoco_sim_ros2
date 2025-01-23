71c078aa72fd455665b9acc83c5b664008096e4b
Ruixiang Du <ruixiang.du@gmail.com>
2025-01-23 22:46:24 +0800

    updated interface

--- include/mujoco_sim_ros2/mujoco_physics_plugin.hpp
+++ include/mujoco_sim_ros2/mujoco_physics_plugin.hpp
@@ -9,17 +9,20 @@
 #ifndef MUJOCO_PHYSICS_PLUGIN_HPP
 #define MUJOCO_PHYSICS_PLUGIN_HPP
 
+#include <rclcpp/rclcpp.hpp>
 #include "mujoco/mujoco.h"
 
-namespace mujoco_ros2 {
+namespace mujoco_sim_ros2 {
 class MujocoPhysicsPlugin {
 public:
   virtual ~MujocoPhysicsPlugin() = default;
 
   // interface
-  virtual void Configure(mjModel *model, mjData *data) = 0;
+  virtual void Configure(rclcpp::Node::SharedPtr & node, rclcpp::NodeOptions cm_node_option,
+                         mjModel *model, mjData *data) = 0;
   virtual void Reset(mjModel *model, mjData *data) = 0;
   virtual void PreUpdate(mjModel *model, mjData *data) {};
+  virtual void Update(mjModel *model, mjData *data) {};
   virtual void PostUpdate(mjModel *model, mjData *data) {};
 
 protected:
